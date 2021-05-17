42afb9d5e0144447f8b5c4182441f13549773b0c
Vanessa Su <vnessa.su@gmail.com>
2021-05-16 20:23:19 -0400

    :hammer: Add boilerplate code

--- Template.Tests/ModelTests/ModelTests.cs
+++ Template.Tests/ModelTests/ModelTests.cs
@@ -1,1 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Template.Models;
+
+namespace Template.Tests
+{
+  [TestClass]
+  public class ModelTests
+  {
+    [TestInitialize]
+    public void Initialize()
+    {
+      //code to run before each test
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      //code to run after each test
+    }
+
+    [TestMethod]
+    public void MethodToTest_Input_ExpectedOutput()
+    {
+      //arrange, act, assert
+    }
+  }
+}
